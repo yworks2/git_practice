322e70e5df85a831d4ae293b8c71e2003b03d197
y-mae <y-mae@192.168.0.132>
2026-02-02 16:45:08 +0900

    2026/2/2 main.java ソース修正

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("これは Git の差分確認用のテストです。");
+		System.out.println("差分テストの追加行です。");
+		System.out.println("差分テストの追加行2つ目です。");
+
 	}
 }
